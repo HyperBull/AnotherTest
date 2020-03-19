596bc532b871ae6f3781e71b408ddd8a569bb94e
Blake Abbott <blake@email.com>
2020-03-19 10:29:58 -0700

    added express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello from app.js');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
