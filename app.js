f1dc6c4a5b484e5b9127ac2227b43d9e0ce4655d
Blake Abbott <blake@email.com>
2020-03-19 12:25:32 -0700

    updated stuff

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'));
-})
+//get static folder
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
