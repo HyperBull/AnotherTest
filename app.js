6165b2ba4e689185a30e700fd69824bf25a3d7fa
Blake Abbott <blake@email.com>
2020-03-23 23:32:27 -0700

    added express and ejs

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+//in terminal type 'npm run dev' to start dev server.
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//get static folder
-app.use(express.static(path.join(__dirname, 'public'))); 
+//set ejs template engine
+app.set('view engine', 'ejs');
 
+//get static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//home
+app.get('/',(req, res) => {
+    console.log(req);
+    res.render('pages/index', {title: 'Home Page'});
+});
+
+//about
+
+
+//listen on ENV.port or port 5000
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
-});
+}); 
